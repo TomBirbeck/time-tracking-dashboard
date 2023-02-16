14d55c0304c7211c95e7c31b52dc6041f8301f46
unknown <tombirbeck47@gmail.com>
2023-02-16 21:09:04 +0000

    hours data dynamically renders on page

--- main.js
+++ main.js
@@ -2,21 +2,27 @@ const titles = document.querySelectorAll('.title')
 const currentArray = document.querySelectorAll('.current')
 const previousArray = document.querySelectorAll('.previous')
 const options = document.querySelectorAll('.options')
+const weekly = document.querySelector('#weekly')
 const res = await fetch ('./data.json')
 const data = await res.json()
 
-console.log(titles)
-
-const handleChange = (value) => {
-    
-}
+console.log(weekly)
 
 const setTitles = (titles, data) => {
-for (let i = 0; i < titles.length; i++){
-    titles[i].textContent = data[i].title
-}
+    for (let i = 0; i < titles.length; i++){
+        titles[i].textContent = data[i].title
+    }
+    }
+    
+    setTitles(titles, data)
+
+const setCardText = (current, previous, data, value) => {
+    for (let i = 0; i < current.length; i++) {
+        current[i].textContent = data[i].timeframes[value].current + 'hrs'
+        previous[i].textContent = 'Last week - ' + data[i].timeframes[value].previous + 'hrs'
+    }
 }
 
-setTitles(titles, data)
+setCardText(currentArray, previousArray, data, 'weekly')
 
-options.forEach((option) => {option.addEventListener('change', () => {console.log(option)})})
+options.forEach((option) => {option.addEventListener('change', () => {setCardText(currentArray, previousArray, data, option.value)})})
